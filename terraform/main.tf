6140decb5e51556a8a7fb74cce9eaa252d12201e
jueun0819 <98609303+jueun0819@users.noreply.github.com>
2022-02-04 21:58:54 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,21 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tamopstf3"
-    container_name       = "tfstatedevops"
-    key                  = "tfstatedevops.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
   }
 }
 
 provider "azurerm" {
+  version = ">=2.0"
+  skip_provider_registration = "true"
   # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
 }
 
-data "azurerm_client_config" "current" {}
-
-#Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
-  location = "eastus2"
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources3"
+  location = "West Europe"
 }
