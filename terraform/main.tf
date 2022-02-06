663f44bc1c11f6768baab614d32fe7371f8bb8e0
jueun0819 <98609303+jueun0819@users.noreply.github.com>
2022-02-05 21:08:22 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources3"
+  name     = "example-resources4"
   location = "West Europe"
 }
